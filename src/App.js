c72af42ee637cf36b29b440e2e7b8b59268ff88b
Sean Luo's Yoga <41530060+lu0x1a0@users.noreply.github.com>
2022-01-05 17:37:41 +1100

    backup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Helmet} from 'react-helmet';
+import AlphabetList from "react-alphabet-list";
+//import randomWords from "random-words";
+//import { Tag, Icon } from "antd";
+//import "antd/dist/antd.css";
+import "./App.css";
 
+
+
+function Entry(props){
+    return (
+        <div>
+            <div className="Entry">
+                {props.item}
+                <hr className = "solid"/>
+            </div>
+        </div>
+    )
+}
 function App() {
+  const data = [
+        'aaa',
+        'bbb',
+        'cbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'dbb',
+        'ebb',
+        'fbb',
+        'gbb',
+        'hbb',
+        'yww'
+    ].concat(["16", "$$$", "😀", "🤣"]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Helmet>
+            <style>{'body { background-color: #222; }'}</style>
+        </Helmet>
+        <AlphabetList
+            className="alpha-list"
+                
+
+            generateFn={(item, index) => {
+            return ( 
+                <div>
+                    <Entry item = {item}/>
+                </div>
+                //<Tag color="rgb(42, 42, 42)" key={item + index}>
+                //  <Icon type="plus" style={{ margin: "0 4px 0 0" }} />
+                //  {item}
+                //</Tag>
+            );
+            }}
+            data={data}
+        />
     </div>
   );
 }
-
 export default App;
